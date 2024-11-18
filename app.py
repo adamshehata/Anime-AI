c538b5c6cc1e4021f332dc93105b6360ac3f606e
adamshehata <146685658+adamshehata@users.noreply.github.com>
2024-11-18 13:26:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ from flask import Flask, render_template, request, jsonify
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-
+# Initialize tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
-
+# Initialize Flask app
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('chat.html')
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -20,23 +19,23 @@ def chat():
     input = msg
     return get_Chat_response(input)
 
-
 def get_Chat_response(text):
+    # Initialize chat_history_ids
+    chat_history_ids = torch.tensor([]).long().unsqueeze(0)  # Empty tensor to store the chat history
 
     # Let's chat for 5 lines
     for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        # Encode the new user input, add the eos_token, and return a tensor in Pytorch
         new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
 
-        # append the new user input tokens to the chat history
+        # Append the new user input tokens to the chat history
         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
 
-        # generated a response while limiting the total chat history to 1000 tokens, 
+        # Generate a response while limiting the total chat history to 1000 tokens
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-        # pretty print last ouput tokens from bot
+        # Return the bot's last output token
         return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
 
 if __name__ == '__main__':
     app.run()
